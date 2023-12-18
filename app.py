02585ea7a9d1ed25b9e10c433f2c20596a3d78e1
Arthur Ferreira <arthur.ferreira@gupy.com.br>
2023-12-18 08:45:27 -0300

    fix: typo

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict():
     features = [float(x) for x in request.form.values()]
     final_features = [features]
     prediction = modelo_final.predict(final_features)
-    return render_template('index.html', predction_text = f'Previsão do Modelo (1- Cliente Fará outra compra / 0 - Cliente Não fará outra compra): {prediction[0]}')
+    return render_template('index.html', prediction_text = f'Previsão do Modelo (1- Cliente Fará outra compra / 0 - Cliente Não fará outra compra): {prediction[0]}')
 
 if __name__ == "__main__":
     app.run(debug = True)
