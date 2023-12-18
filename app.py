adba304cb59278fd8d43e1d4df94d8734f48c77b
Arthur Ferreira <arthur.ferreira@gupy.com.br>
2023-12-18 08:19:24 -0300

    add flask API

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__)
+
+modelo_final = joblib.load('modelo_treinado.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [features]
+    prediction = modelo_final.predict(final_features)
+    return render_template('index.html', predction_text = f'Previsão do Modelo (1- Cliente Fará outra compra / 0 - Cliente Não fará outra compra): {prediction[0]}')
+
+if __name__ == "__main__":
+    app.run(debug = True)
